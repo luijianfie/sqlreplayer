7932026a01593ac063699beb1426ec36c1abc1bc
luijianfie <luwenhaoterry@163.com>
2025-01-10 18:16:52 +0800

    去除几个分位值，确保在大批量的回访任务下面内存消耗稳定

--- sqlreplayer.go
+++ sqlreplayer.go
@@ -86,6 +86,19 @@ type task struct {
 	connsToReplayRows int64
 }
 
+type sqlStatus struct {
+	sqlid       string
+	fingerprint string
+	Tablelist   string
+	Sqltype     string
+	min         uint64
+	max         uint64
+	timeElapse  uint64
+	minsql      string
+	maxsql      string
+	execution   uint64
+}
+
 type SQLReplayer struct {
 	JobSeq  uint64 //jobSeq is global uniq id
 	TaskSeq int    //taskSeq is uniq id in a job
@@ -123,14 +136,15 @@ type SQLReplayer struct {
 	ReplayStatFileDir string
 	replayStatFile    *os.File
 
-	SqlID2Fingerprint map[string]string // map[SQLID] -> fingerprint
+	SqlID2Fingerprint map[string]*sqlStatus // map[SQLID] -> fingerprint
 
-	//analyze pharse for generating report
-	SqlID2ReplayUints map[string][]replayUnit // map[SQLID] -> [ru1,ru2,ru3,ru4...]
+	// //analyze pharse for generating report
+	// SqlID2ReplayUints map[string][]replayUnit // map[SQLID] -> [ru1,ru2,ru3,ru4...]
 
+	QueryID2RelayStats map[string][]*sqlStatus
 	//replayer pharse for generating report
 	//map[SQLID]->[map[db1],map[db2]...]       map[db1]->[ru1,ru2,ru3,ru4...]
-	QueryID2RelayStats map[string][][]replayUnit
+	// QueryID2RelayStats map[string][][]replayUnit
 
 	logger *zap.Logger `json:"-"`
 
@@ -173,14 +187,13 @@ func NewSQLReplayer(jobSeq uint64, c *model.Config) (*SQLReplayer, error) {
 			JobSeq:             jobSeq,
 			LogType:            c.LogType,
 			Current:            time.Now(),
-			SqlID2Fingerprint:  make(map[string]string),
+			SqlID2Fingerprint:  make(map[string]*sqlStatus),
 			SaveRawSQLInReport: c.SaveRawSQLInReport,
 			GenerateReport:     c.GenerateReport,
 			QueryOnly:          c.QueryOnly,
 			DryRun:             c.DryRun,
 			DrawPic:            c.DrawPic,
-			SqlID2ReplayUints:  make(map[string][]replayUnit),
-			QueryID2RelayStats: make(map[string][][]replayUnit),
+			QueryID2RelayStats: make(map[string][]*sqlStatus),
 			Currency:           c.WorkerNum,
 			Multiplier:         c.Multi,
 			Thread:             c.Thread,
@@ -545,8 +558,8 @@ func (sr *SQLReplayer) analyze(t *task) error {
 	csvWriter := csv.NewWriter(rawSQLFile)
 	defer csvWriter.Flush()
 
-	tmpSqlID2Fingerprint := make(map[string]string)
-	tmpSqlID2ReplayUints := make(map[string][]replayUnit)
+	//save sqlid to finggerprint
+	tmpSqlID2Fingerprint := make(map[string]*sqlStatus)
 
 	//deal with command unit
 	curPos, err := sr.parser.Parser(file, pos, func(cu *model.CommandUnit) error {
@@ -570,25 +583,50 @@ func (sr *SQLReplayer) analyze(t *task) error {
 		cu.QueryID, fingerprint = utils.GetQueryID(cu.Argument)
 		cu.TableList, _ = utils.ExtractTableNames(cu.Argument)
 
-		_, ok := tmpSqlID2Fingerprint[cu.QueryID]
-		if !ok {
-			tmpSqlID2Fingerprint[cu.QueryID] = fingerprint
-		}
-
 		//save to raw sql file
 		err := csvWriter.Write([]string{cu.Argument, cu.QueryID, cu.Time.Format("20060102 15:04:05"), cu.CommandType, cu.TableList, strconv.FormatFloat(cu.Elapsed*1000, 'f', 2, 64)})
 		if err != nil {
 			return err
 		}
 
-		if sr.GenerateReport {
-			ru := replayUnit{Sqlid: cu.QueryID, Tablelist: cu.TableList, Sqltype: cu.CommandType, Time: uint64(cu.Elapsed * 1000)}
-
-			if sr.SaveRawSQLInReport {
-				ru.Sql = cu.Argument
+		_, ok := tmpSqlID2Fingerprint[cu.QueryID]
+		if !ok {
+			s := &sqlStatus{
+				sqlid:       cu.QueryID,
+				fingerprint: fingerprint,
+				execution:   1,
+				timeElapse:  uint64(cu.Elapsed * 1000),
+				Tablelist:   cu.TableList,
+				min:         uint64(cu.Elapsed * 1000),
+				max:         uint64(cu.Elapsed * 1000),
 			}
-			tmpSqlID2ReplayUints[cu.QueryID] = append(tmpSqlID2ReplayUints[cu.QueryID], ru)
+			if sr.GenerateReport {
+				s.minsql = cu.Argument
+				s.maxsql = cu.Argument
+			}
+			tmpSqlID2Fingerprint[cu.QueryID] = s
+		} else {
+
+			elapse := uint64(cu.Elapsed * 1000)
+			s := tmpSqlID2Fingerprint[cu.QueryID]
+			s.execution++
+			s.timeElapse += elapse
+
+			if s.min > elapse {
+				s.min = elapse
+				if sr.GenerateReport {
+					s.minsql = cu.Argument
+				}
+			}
+
+			if s.max < elapse {
+				s.max = elapse
+				if sr.GenerateReport {
+					s.maxsql = cu.Argument
+				}
+			}
 		}
+
 		return nil
 
 	})
@@ -596,14 +634,30 @@ func (sr *SQLReplayer) analyze(t *task) error {
 	t.parseFilePos = curPos
 
 	sr.mutex.Lock()
+	if len(tmpSqlID2Fingerprint) == 0 {
+		fmt.Println(len(tmpSqlID2Fingerprint))
+	}
 	for k, v := range tmpSqlID2Fingerprint {
-		_, ok := tmpSqlID2Fingerprint[k]
+		_, ok := sr.SqlID2Fingerprint[k]
 		if !ok {
 			sr.SqlID2Fingerprint[k] = v
+		} else {
+			s := sr.SqlID2Fingerprint[k]
+
+			s.execution += v.execution
+			s.timeElapse += v.timeElapse
+
+			if s.min > v.min {
+				s.min = v.min
+				s.minsql = v.minsql
+			}
+
+			if s.max < v.max {
+				s.max = v.max
+				s.maxsql = v.maxsql
+			}
+
 		}
-	}
-	for k, slices := range tmpSqlID2ReplayUints {
-		sr.SqlID2ReplayUints[k] = append(sr.SqlID2ReplayUints[k], slices...)
 	}
 
 	sr.mutex.Unlock()
@@ -628,11 +682,6 @@ func (sr *SQLReplayer) generateRawSQLReport() error {
 
 	header := []string{"sqlid", "fingerprint", "sqltype", "tablelist"}
 	subHeaders := []string{"min(ms)", "min-sql",
-		"p25(ms)", "p25-sql",
-		"p50(ms)", "p50-sql",
-		"p75(ms)", "p75-sql",
-		"p90(ms)", "p90-sql",
-		"p99(ms)", "p99-sql",
 		"max(ms)", "max-sql",
 		"avg(ms)", "execution"}
 	header = append(header, subHeaders...)
@@ -642,18 +691,13 @@ func (sr *SQLReplayer) generateRawSQLReport() error {
 		panic(err)
 	}
 
-	for sqlID, sqls := range sr.SqlID2ReplayUints {
-		row := []string{sqlID, sr.SqlID2Fingerprint[sqlID], sqls[0].Sqltype, sqls[0].Tablelist}
-		srMin, sr25, sr50, sr75, sr90, sr99, srMax, count, avg, _ := analyzer(sqls)
+	for sqlID, sqlStatus := range sr.SqlID2Fingerprint {
+		row := []string{sqlID, sqlStatus.fingerprint, sqlStatus.Sqltype, sqlStatus.Tablelist}
+
 		row = append(row, []string{
-			strconv.FormatUint(srMin.Time, 10), srMin.Sql,
-			strconv.FormatUint(sr25.Time, 10), sr25.Sql,
-			strconv.FormatUint(sr50.Time, 10), sr50.Sql,
-			strconv.FormatUint(sr75.Time, 10), sr75.Sql,
-			strconv.FormatUint(sr90.Time, 10), sr90.Sql,
-			strconv.FormatUint(sr99.Time, 10), sr99.Sql,
-			strconv.FormatUint(srMax.Time, 10), srMax.Sql,
-			strconv.FormatFloat(avg, 'f', 2, 64), strconv.FormatUint(count, 10)}...)
+			strconv.FormatUint(sqlStatus.min, 10), sqlStatus.minsql,
+			strconv.FormatUint(sqlStatus.max, 10), sqlStatus.maxsql,
+			strconv.FormatFloat(float64(sqlStatus.timeElapse)/float64(sqlStatus.execution), 'f', 2, 64), strconv.FormatUint(sqlStatus.execution, 10)}...)
 		err = writer.Write(row)
 		if err != nil {
 			return err
@@ -802,14 +846,14 @@ func (sr *SQLReplayer) replayRawSQL(t *task) error {
 			sr.mutex.Lock()
 			sr.ReplayRowCount++
 
-			_, ok := sr.SqlID2Fingerprint[sqlid]
-			if !ok {
-				sr.SqlID2Fingerprint[sqlid] = fingerprint
-			}
+			// _, ok := sr.SqlID2Fingerprint[sqlid]
+			// if !ok {
+			// 	sr.SqlID2Fingerprint[sqlid] = &sqlStatus{fingerprint: fingerprint}
+			// }
 
-			_, ok = sr.QueryID2RelayStats[sqlid]
+			_, ok := sr.QueryID2RelayStats[sqlid]
 			if !ok {
-				sr.QueryID2RelayStats[sqlid] = make([][]replayUnit, len(sr.Conns))
+				sr.QueryID2RelayStats[sqlid] = make([]*sqlStatus, len(sr.Conns))
 			}
 
 			sr.mutex.Unlock()
@@ -823,8 +867,6 @@ func (sr *SQLReplayer) replayRawSQL(t *task) error {
 
 					for ind, db := range dbs {
 
-						ru := replayUnit{}
-
 						start := time.Now()
 
 						_, err := db.Exec(sql)
@@ -837,14 +879,43 @@ func (sr *SQLReplayer) replayRawSQL(t *task) error {
 						elapsed := time.Since(start)
 						elapsedMilliseconds := elapsed.Milliseconds()
 
-						ru.Time = uint64(elapsedMilliseconds)
-						ru.Tablelist = tablelist
-						if sr.SaveRawSQLInReport {
-							ru.Sql = sql
-						}
 						sr.mutex.Lock()
-						conns2ReplayStats := sr.QueryID2RelayStats[sqlid]
-						conns2ReplayStats[ind] = append(conns2ReplayStats[ind], ru)
+
+						if sr.QueryID2RelayStats[sqlid][ind] == nil {
+							s := &sqlStatus{
+								sqlid:       sqlid,
+								fingerprint: fingerprint,
+								Tablelist:   tablelist,
+								execution:   1,
+								timeElapse:  uint64(elapsedMilliseconds),
+								min:         uint64(elapsedMilliseconds),
+								max:         uint64(elapsedMilliseconds),
+							}
+
+							if sr.SaveRawSQLInReport {
+								s.minsql = sql
+								s.maxsql = sql
+							}
+							sr.QueryID2RelayStats[sqlid][ind] = s
+						} else {
+							s := sr.QueryID2RelayStats[sqlid][ind]
+							s.execution += 1
+							s.timeElapse += uint64(elapsedMilliseconds)
+
+							if s.min > uint64(elapsedMilliseconds) {
+								s.min = uint64(elapsedMilliseconds)
+								if sr.SaveRawSQLInReport {
+									s.minsql = sql
+								}
+							}
+
+							if s.max < uint64(elapsedMilliseconds) {
+								s.max = uint64(elapsedMilliseconds)
+								if sr.SaveRawSQLInReport {
+									s.maxsql = sql
+								}
+							}
+						}
 						sr.mutex.Unlock()
 					}
 
@@ -915,11 +986,6 @@ func generateReplayReport(sr *SQLReplayer) error {
 		for i := 0; i < len(sr.Conns); i++ {
 			prefix := fmt.Sprintf("conn_%d_", i)
 			subHeaders := []string{prefix + "min(ms)", prefix + "min-sql",
-				prefix + "p25(ms)", prefix + "p25-sql",
-				prefix + "p50(ms)", prefix + "p50-sql",
-				prefix + "p75(ms)", prefix + "p75-sql",
-				prefix + "p90(ms)", prefix + "p90-sql",
-				prefix + "p99(ms)", prefix + "p99-sql",
 				prefix + "max(ms)", prefix + "max-sql",
 				prefix + "avg(ms)", prefix + "execution"}
 			header = append(header, subHeaders...)
@@ -933,30 +999,20 @@ func generateReplayReport(sr *SQLReplayer) error {
 
 			//in some cases, failed to execute sql
 			//dbsToStats[0] may be nil,skip this
-			if dbsToStats[0] == nil {
+			if dbsToStats == nil {
 				continue
 			}
-			row := []string{sqlid, sr.SqlID2Fingerprint[sqlid], dbsToStats[0][0].Sqltype, dbsToStats[0][0].Tablelist}
+			row := []string{sqlid, dbsToStats[0].fingerprint, dbsToStats[0].Sqltype, dbsToStats[0].Tablelist}
 			for i := 0; i < len(dbsToStats); i++ {
-				srMin, sr25, sr50, sr75, sr90, sr99, srMax, count, avg, seqs := analyzer(dbsToStats[i])
 				row = append(row, []string{
-					strconv.FormatUint(srMin.Time, 10), srMin.Sql,
-					strconv.FormatUint(sr25.Time, 10), sr25.Sql,
-					strconv.FormatUint(sr50.Time, 10), sr50.Sql,
-					strconv.FormatUint(sr75.Time, 10), sr75.Sql,
-					strconv.FormatUint(sr90.Time, 10), sr90.Sql,
-					strconv.FormatUint(sr99.Time, 10), sr99.Sql,
-					strconv.FormatUint(srMax.Time, 10), srMax.Sql,
-					strconv.FormatFloat(avg, 'f', 2, 64), strconv.FormatUint(count, 10)}...)
-
-				if sr.DrawPic {
-
-					fname := "Conn" + strconv.Itoa(i) + "_" + sqlid
-					utils.Draw(fname, sr.Dir+"/"+fname, seqs, float64(sr25.Time), float64(sr50.Time), float64(sr75.Time), float64(sr90.Time))
-					sr.logger.Sugar().Infoln("draw picture for sqlid:" + sqlid + " to " + sr.Dir + "/" + fname + ".png.")
-
-				}
-
+					strconv.FormatUint(dbsToStats[i].min, 10), dbsToStats[i].minsql,
+					strconv.FormatUint(dbsToStats[i].max, 10), dbsToStats[i].maxsql,
+					strconv.FormatFloat(float64(dbsToStats[i].timeElapse)/float64(dbsToStats[i].execution), 'f', 2, 64), strconv.FormatUint(dbsToStats[i].execution, 10)}...)
+				// if sr.DrawPic {
+				// 	fname := "Conn" + strconv.Itoa(i) + "_" + sqlid
+				// 	utils.Draw(fname, sr.Dir+"/"+fname, seqs, float64(sr25.Time), float64(sr50.Time), float64(sr75.Time), float64(sr90.Time))
+				// 	sr.logger.Sugar().Infoln("draw picture for sqlid:" + sqlid + " to " + sr.Dir + "/" + fname + ".png.")
+				// }
 			}
 			err = writer.Write(row)
 			if err != nil {
