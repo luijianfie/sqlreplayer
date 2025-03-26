e7a618defe6cc55063adda70621dafe1cfb83101
luijianfie <luwenhaoterry@163.com>
2025-03-26 18:16:26 +0800

    增加适配mysql5.7.44版本下面类似2025-03-25T03:46:22.135744Z这样的时间格式

--- cmd/main.go
+++ cmd/main.go
@@ -17,7 +17,7 @@ import (
 	"gopkg.in/yaml.v2"
 )
 
-const version = "3.0.1"
+const version = "3.0.2"
 
 var (
 	execType             string
--- parser/parser.go
+++ parser/parser.go
@@ -25,7 +25,7 @@ var (
 		}, "|") + `)$`)
 	reGenlog56 *regexp.Regexp = regexp.MustCompile(`(?s)^(\d{6}\s+\d{1,2}:\d{2}:\d{2}|\t)\s+(\d+)\s+([^\t]+)\t(.*)`)
 
-	reGenlog57 *regexp.Regexp = regexp.MustCompile(`(?s)^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})\.\d{6}\+\d{2}:\d{2}\s+(\d+)\s+([^\t]+)\t(.*)`)
+	reGenlog57 *regexp.Regexp = regexp.MustCompile(`(?s)^(\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2})\.\d{6}(?:(?:\+\d{2}:\d{2})|Z)\s+(\d+)\s+([^\t]+)\t(.*)`)
 
 	slowlogReIgnore *regexp.Regexp = regexp.MustCompile(
 		`^(?s)(?:` + strings.Join([]string{
